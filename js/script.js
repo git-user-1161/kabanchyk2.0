14647fbc897ba29f6944ec8067554a85ceb4ec45
git-user-1161 <palarmisha2007@gmail.com>
2024-07-16 18:32:33 +0300

    final product

--- js/script.js
+++ js/script.js
@@ -39,7 +39,8 @@ class LikedWorker {
         this.saveCartToCookies()
     }
     removeItem(item){
-        delete items[item.name]
+        delete this.items[item.name]
+        this.saveCartToCookies()
     }
     saveCartToCookies() {
         let cartJSON = JSON.stringify(this.items);
@@ -110,10 +111,17 @@ getProducts().then(function (products) {
     if(likes){
         likes.forEach(function (like) {
             like.addEventListener('click', function (event) {
-                like.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" /></svg>'
+                
                 let itemdata = like.getAttribute('data-item')
                 let item = JSON.parse(itemdata)
-                likedItems.addItem(item)
+                if(likedItems.items[item.name]){
+                    likedItems.removeItem(item)
+                    like.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>'
+                }else{
+                    likedItems.addItem(item)
+                    like.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" /></svg>'
+                }
+                
             })
         })
     }

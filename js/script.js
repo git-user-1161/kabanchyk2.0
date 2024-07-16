1089a806b21b98a12c6a5d02cf4ba5c911e9a701
git-user-1161 <palarmisha2007@gmail.com>
2024-07-16 17:57:57 +0300

    add to cart

--- js/script.js
+++ js/script.js
@@ -6,9 +6,57 @@ let home = document.querySelector('.home')
 let orderblock = document.querySelector('.order-form')
 let likedWorker = document.querySelector('.liked-workers')
 let cartBlock = document.querySelector('.cart')
+let likedBlock = document.querySelector('.like-list')
+function getCookieValue(cookieName) {
+    // Розділяємо всі куки на окремі частини
+    const cookies = document.cookie.split(';')
+    // Шукаємо куки з вказаним ім'ям
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim() // Видаляємо зайві пробіли
+        // Перевіряємо, чи починається поточне кукі з шуканого імені
+        if (cookie.startsWith(cookieName + '=')) {
+            // Якщо так, повертаємо значення кукі
+            return cookie.substring(cookieName.length + 1) // +1 для пропуску символу
+            "="
+        }
+    }
+
+    return ''
+}
 
 
 
+class LikedWorker {
+    constructor() {
+        this.items = {}  // усі товари
+        this.loadCartFromCookies()
+
+    }
+
+    // Додавання товару до кошика
+    addItem(item) {
+        this.items[item.name] = item
+        this.saveCartToCookies()
+    }
+    removeItem(item){
+        delete items[item.name]
+    }
+    saveCartToCookies() {
+        let cartJSON = JSON.stringify(this.items);
+        document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`;
+    }
+    // Завантаження кошика з кукі
+    loadCartFromCookies() {
+        let cartCookie = getCookieValue('cart');
+        if (cartCookie && cartCookie !== '') {
+            this.items = JSON.parse(cartCookie);
+
+        }
+    }
+
+}
+let likedItems = new LikedWorker()
+console.log(likedItems)
 async function getProducts() {
     // Виконуємо запит до файлу "store_db.json" та очікуємо на відповідь
     let response = await fetch("db.json")
@@ -25,6 +73,19 @@ function openOrderForm(productName) {
     document.getElementById('product').value = productName;
 }
 function getCardHTML(product) {
+    if (likedItems.items[product.name]){
+        return `
+        <div class="card">
+            <h3>${product.name}</h3>
+            <img src="img/${product.image}" alt="${product.name}">
+            <p>${product.description}</p>
+            <div class='btns'>
+            <button onclick="openOrderForm('${product.name}')">Order</button>
+            <button class="btn like-btn" data-item='${JSON.stringify(product)}'>
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" /></svg></button></div>
+        </div>
+        `
+    }
     return `
     <div class="card">
         <h3>${product.name}</h3>
@@ -43,15 +104,20 @@ getProducts().then(function (products) {
             // Відображаємо товари на сторінці
             tovar.innerHTML += getCardHTML(product)
         })
+
     }
     let likes = document.querySelectorAll('.like-btn')
-    likes.forEach(function(like){
-        like.addEventListener('click',function () {
-            like.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" /></svg>'
-            let itemdata = event.target.getAttribute('data-item')
-            let product = JSON.parse(itemdata)
+    if(likes){
+        likes.forEach(function (like) {
+            like.addEventListener('click', function (event) {
+                like.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" /></svg>'
+                let itemdata = like.getAttribute('data-item')
+                let item = JSON.parse(itemdata)
+                likedItems.addItem(item)
+            })
         })
-    })
+    }
+
     // Отримуємо всі кнопки "Купити" на сторінці
     // let buyButtons = document.querySelectorAll('.products-list .cart-btn');
     // // Навішуємо обробник подій на кожну кнопку "Купити"
@@ -79,22 +145,27 @@ function checkSiteInfo() {
     tovar.style.display = 'none';
     info.style.display = 'block';
     orderblock.style.display = 'none';
-    cartBlock.style.display ='none'
+    cartBlock.style.display = 'none'
 
 }
 function hideSiteInfo() {
     tovar.style.display = 'block';
     info.style.display = 'none';
     orderblock.style.display = 'none';
-    cartBlock.style.display ='none'
+    cartBlock.style.display = 'none'
 
 
 }
 function showLikedWorkers() {
+    cartBlock.innerHTML = ''
+    for(let name in likedItems.items){
+        cartBlock.innerHTML += getCardHTML(likedItems.items[name])
+    }
     tovar.style.display = 'none';
     info.style.display = 'none';
     orderblock.style.display = 'none';
-    cartBlock.style.display ='block'
+    cartBlock.style.display = 'block'
+
 
 
 }
@@ -103,5 +174,5 @@ home.addEventListener('click', hideSiteInfo)
 likedWorker.addEventListener('click', showLikedWorkers)
 
 anime({
-    targets:'like-btn'
+    targets: 'like-btn'
 })
